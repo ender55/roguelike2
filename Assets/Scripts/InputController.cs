3ee0da95ef15d76b29e718f32d2dc41f902ed4b5
ender <andrei.07062001@gmail.com>
2023-05-03 20:11:53 +0600

    [ADD] InputController for input handling

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour, GameInput.IGameplayActions, GameInput.IUIActions
+{
+    private GameInput _gameInput;
+    
+    public event Action<Vector2> OnMove;
+
+    private void Awake()
+    {
+        _gameInput = new GameInput();
+        _gameInput.Gameplay.SetCallbacks(this);
+        _gameInput.UI.SetCallbacks(this);
+        SetActionMap("Gameplay");
+    }
+
+    private void OnEnable()
+    {
+        _gameInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _gameInput.Disable();
+    }
+
+    public void SetActionMap(string newInputActionMap)
+    {
+        foreach (InputActionMap inputActionMap in _gameInput.asset.actionMaps)
+        {
+            if(inputActionMap.name == newInputActionMap)
+            {
+                inputActionMap.Enable();
+            }
+            else
+            {
+                inputActionMap.Disable();
+            }
+        }
+    }
+    
+    void GameInput.IGameplayActions.OnMove(InputAction.CallbackContext context)
+    {
+        OnMove?.Invoke(context.ReadValue<Vector2>());
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private InputController _inputController;
+
+    private void Awake()
+    {
+        _inputController = gameObject.AddComponent<InputController>();
+    }
+
+    private void OnEnable()
+    {
+        _inputController.OnMove += Move;
+    }
+
+    private void OnDisable()
+    {
+        _inputController.OnMove -= Move;
+    }
+
+    private void Move(Vector2 moveDirection)
+    {
+        Debug.Log(moveDirection);
+    }
+}
