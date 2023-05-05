742b092d7af8662b14cc5d556840a6ab3ad4552b
ender <andrei.07062001@gmail.com>
2023-05-05 15:59:49 +0600

    [ADD] health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class Health
+{
+    [SerializeField] private int currentHp;
+    [SerializeField] private int maxHp;
+    
+    public int CurrentHp => currentHp;
+    public int MaxHp => maxHp;
+    
+    public event Action OnHpChange;
+    public event Action OnMaxHpChange;
+    public event Action OnZeroHp;
+    
+
+    public void ChangeCurrentHp(int value)
+    {
+        int newCurrentHp = currentHp + value;
+        if(newCurrentHp <= 0)
+        {
+            currentHp = 0;
+            OnZeroHp?.Invoke();
+        }
+        else if(newCurrentHp > maxHp)
+        {
+            currentHp = maxHp;
+        }
+        else
+        {
+            currentHp = newCurrentHp;
+        }
+        OnHpChange?.Invoke();
+    }
+
+    public void ChangeMaxHp(int value)
+    {
+        int newMaxHp = maxHp + value;
+        if(newMaxHp < currentHp)
+        {
+            SetCurrentHp(newMaxHp);
+        }
+        maxHp = newMaxHp;
+        OnMaxHpChange?.Invoke();
+    }
+
+    public void SetCurrentHp(int value)
+    {
+        int newCurrentHp = value;
+        if(newCurrentHp <= 0)
+        {
+            currentHp = 0;
+            OnZeroHp?.Invoke();
+        }
+        else if(newCurrentHp > maxHp)
+        {
+            currentHp = maxHp;
+        }
+        else
+        {
+            currentHp = newCurrentHp;
+        }
+        OnHpChange?.Invoke();
+    }
+    
+    public void SetMaxHp(int value)
+    {
+        int newMaxHp = value;
+        if(newMaxHp < currentHp)
+        {
+            SetCurrentHp(newMaxHp);
+        }
+        maxHp = newMaxHp;
+        OnMaxHpChange?.Invoke();
+    }
+}
